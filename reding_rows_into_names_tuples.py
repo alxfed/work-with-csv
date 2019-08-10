8b494d1218a54dafc27154512133bf09c5763409
Alex Fedotov <alxfed@gmail.com>
2019-08-10 07:27:38 -0500

    reading csv rows into named tuples

--- reding_rows_into_names_tuples.py
+++ reding_rows_into_names_tuples.py
@@ -6,10 +6,10 @@ file_path = '/media/alxfed/toca/aa-crm/preparation/unknown_companies.csv'
 
 with open(file_path) as f:
     f_csv = csv.reader(f)
-    headings = next(f_csv)
-    # replace spaces in headings with underscores
-    headings = [heading.replace(' ', '_') for heading in headings]
-    Row = namedtuple('Row', headings)
+    headers = next(f_csv)
+    # replace spaces in headers with underscores
+    headers = [header.replace(' ', '_') for header in headers]
+    Row = namedtuple('Row', headers)
     for r in f_csv:
         row = Row(*r)
         a = row.Name
