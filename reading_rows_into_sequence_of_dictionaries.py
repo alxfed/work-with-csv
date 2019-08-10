30646acc36a232d5088074adc96a791e4e74dec2
Alex Fedotov <alxfed@gmail.com>
2019-08-10 07:41:01 -0500

    reading csv rows into named tuples

---
+++ reading_rows_into_sequence_of_dictionaries.py
@@ -0,0 +1,10 @@
+import csv
+
+
+file_path = '/media/alxfed/toca/aa-crm/preparation/unknown_companies.csv'
+
+with open(file_path) as f:
+    f_csv = csv.DictReader(f)
+    for row in f_csv:
+        a = row['Name']
+        b = row['Street Address']
