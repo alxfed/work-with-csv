6acea5673f5cea92e82b425f28c163fa0536012f
Alex Fedotov <alxfed@gmail.com>
2019-08-10 15:44:44 -0500

    simple utilities

--- reading_rows_into_named_tuples.py
+++ reading_rows_into_named_tuples.py
@@ -3,6 +3,7 @@ reading rows of csv into (immutable) named tuples
 """
 import csv
 from collections import namedtuple
+import re
 
 
 file_path = '/media/alxfed/toca/aa-crm/preparation/unknown_companies.csv'
@@ -11,10 +12,10 @@ with open(file_path) as f:
     f_csv = csv.reader(f)
     headers = next(f_csv)
     # replace spaces in headers with underscores
-    headers = [header.replace(' ', '_') for header in headers]
+    # headers = [header.replace(' ', '_') for header in headers]
     # a full blown version with an imported re is:
-    # headers = [ re.sub('[^a-zA-Z_]', '_', h) for h in next(f_csv) ]
-    Row = namedtuple('Row', headers)
+    tuple_headers = [re.sub('[^a-zA-Z_]', '_', h) for h in next(f_csv)]
+    Row = namedtuple('Row', tuple_headers)
     for r in f_csv:
         row = Row(*r)
         a = row.Name
