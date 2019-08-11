91fb6164aa009a9a1db72c5d911e3b9963f445cc
alxfed <alxfed@gmail.com>
2019-08-11 10:16:55 -0500

    simple utilities

--- reading_rows_into_list_of_dictionaries.py
+++ reading_rows_into_list_of_dictionaries.py
@@ -5,9 +5,13 @@ import csv
 
 
 file_path = '/media/alxfed/toca/aa-crm/preparation/unknown_companies.csv'
+fieldnames = []
 
 with open(file_path) as f:
     f_csv = csv.DictReader(f)
     for row in f_csv:
         a = row['Name']
         b = row['Street Address']
+    fieldnames = f_csv._fieldnames  # after reading all the rows it borrows this
+                                    # 'protected field' from the object. Otherwise
+                                    # it is unclear how to set the headers for writing.
---
+++ simple-utilities/synthesizing_values_from_other_columns.py
@@ -0,0 +1,39 @@
+"""
+given: a csv file with a list of objects that have no value of a
+    property
+needed: a csv file with objects that have this value sythesized from
+    the values taken from other columns according to a rule
+"""
+import csv
+
+
+read_path = '/media/alxfed/toca/aa-crm/preparation/companies_without_phone.csv'
+write_path = '/media/alxfed/toca/aa-crm/preparation/companies_with_added_phone.csv'
+
+fieldnames = []
+rows = []
+
+with open(read_path) as f:
+    f_csv = csv.DictReader(f)
+    for row in f_csv:
+        if row['Phone Landline']:
+            row['Phone Number'] = row['Phone Landline']
+        if row['Phone Mobile'] and not row['Phone Number']:
+            row['Phone Number'] = row['Phone Mobile']
+        if row['Phone VoIP'] and not row['Phone Number']:
+            row['Phone Number'] = row['Phone VoIP']
+        if row['Phone Toll'] and not row['Phone Number']:
+            row['Phone Number'] = row['Phone Toll']
+        if row['Phone Unidentified'] and not row['Phone Number']:
+            row['Phone Number'] = row['Phone Unidentified']
+        rows.append(row)
+    fieldnames = f_csv._fieldnames
+
+write_headers = fieldnames
+write_rows = rows
+
+
+with open(write_rows,'w') as f:
+    f_csv = csv.DictWriter(f, write_headers)
+    f_csv.writeheader()
+    f_csv.writerows(write_rows)
