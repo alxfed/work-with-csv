fd32d8a4e8536c6b99a573142221a28a3de1bfcc
alxfed <alxfed@gmail.com>
2019-08-15 20:12:09 -0500

    simple utilities

--- simple-utilities/find_emails_of_a_website.py
+++ simple-utilities/find_emails_of_a_website.py
@@ -13,7 +13,8 @@ import requests
 
 # constants
 file_path = '/media/alxfed/toca/aa-crm/other-lists/08122019_archs_interor.csv'
-output_file_path = '/media/alxfed/toca/aa-crm/other-lists/08122019_archs_with_emails.csv'
+output_file_path = '/media/alxfed/toca/aa-crm/other-lists/output_archs_with_emails.csv'
+final_file_path = '/media/alxfed/toca/aa-crm/other-lists/08122019_archs_with_emails.csv'
 credits_check_url = 'https://api.anymailfinder.com/v4.1/account/hits_left.json'
 api_url = 'https://api.anymailfinder.com/v4.1/search/company.json'
 api_key = environ['API_KEY']
@@ -45,43 +46,56 @@ fieldnames = ['Name', 'Phone Mobile', 'Phone Voip', 'Phone Toll',
 
 with open(file_path) as f:
     f_csv = csv.DictReader(f, restkey='Rest', restval='')
-    for row in f_csv:
-        website = row['Website']
-        if not website:  # check whether there is a website
-            row.update({'emails': ''})
-            row.update({'email_class': ''})
-            pass
-        else:
-            tsd, td, tsu = extract(website)  # tldextract
-            payload = {'domain': td + '.' + tsu,
-                       'company_name': row['Name']}
-            r = requests.post(api_url, headers=headers,
-                              json=payload, timeout=60)
-            if r.status_code > 202:
-                """errors
-                """
-                print('Wow! Errors happened!', r.status_code)
-                pass
-            elif r.status_code < 203:
-                while r.status_code != 200:
-                    time.sleep(3)
-                    r = requests.request('POST', api_url,
-                                         headers=headers, json=payload)
-                    if r.status_code == 200:
-                        break
-                resp = r.json()
-                row.update({'emails': " ".join(resp['emails'])})
-                row.update({'email_class': resp['email_class']})
-                print(row['Name'], row['emails'], row['email_class'])
+    with open(output_file_path, 'w') as wr:
+        wr_csv = csv.DictWriter(wr, fieldnames=fieldnames)
+        wr_csv.writeheader()
+        for row in f_csv:
+            website = row['Website']
+            if not website:  # check whether there is a website
+                row.update({'emails': ''})
+                row.update({'email_class': ''})
                 pass
             else:
-                print('I dunno what this is...', r.status_code)
-                pass
-        rows.append(row)
+                tsd, td, tsu = extract(website)  # tldextract
+                payload = {'domain': td + '.' + tsu,
+                           'company_name': row['Name']}
+                r = requests.post(api_url, headers=headers,
+                                  json=payload, timeout=60)
+                if r.status_code > 202:
+                    """errors
+                    """
+                    print('Wow! Errors happened!', r.status_code)
+                    pass
+                elif r.status_code < 203:
+                    timeout = False; attempts = 0
+                    while r.status_code != 200:
+                        time.sleep(3)
+                        r = requests.request('POST', api_url,
+                                             headers=headers, json=payload)
+                        if r.status_code == 200:
+                            break
+                        else:
+                            attempts += 1
+                            print(attempts)
+                            if attempts > 10:
+                                timeout = True
+                                break
+                    if not timeout:
+                        resp = r.json()
+                        row.update({'emails': " ".join(resp['emails'])})
+                        row.update({'email_class': resp['email_class']})
+                        print(row['Name'], row['emails'], row['email_class'])
+                    else:
+                        print(row['Name'], 'Timeout')
+                else:
+                    print('I dunno what this is...', r.status_code)
+                    pass
+            wr_csv.writerow(row)
+            rows.append(row)
     # fieldnames = f_csv._fieldnames
     # fieldnames.extend(['emails', 'email_class'])
 
-with open(output_file_path,'w') as f:
+with open(final_file_path,'w') as f:
     f_csv = csv.DictWriter(f, fieldnames=fieldnames)
     f_csv.writeheader()
     f_csv.writerows(rows)
