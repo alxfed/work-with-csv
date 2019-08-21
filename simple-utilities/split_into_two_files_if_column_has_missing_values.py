49a05e13c43a461477b4ea331f5f0b69472d43ef
alxfed <alxfed@gmail.com>
2019-08-21 18:05:50 -0500

    finding values for additional column in another file

--- simple-utilities/split_into_two_files_if_column_has_missing_values.py
+++ simple-utilities/split_into_two_files_if_column_has_missing_values.py
@@ -8,8 +8,8 @@ import re
 from collections import namedtuple
 
 read_path = '/media/alxfed/toca/aa-crm/enrich/kitchen_and_bath_designers.csv'
-write_path = '/media/alxfed/toca/aa-crm/other-lists/kitchen_and_bath_designers_with_websites.csv'
-write_excluded_path = '/media/alxfed/toca/aa-crm/other-lists/kitchen_and_bath_designers_without_websites.csv'
+write_path = '/media/alxfed/toca/aa-crm/enrich/kitchen_and_bath_designers_with_websites.csv'
+write_excluded_path = '/media/alxfed/toca/aa-crm/enrich/kitchen_and_bath_designers_without_websites.csv'
 
 rows = []
 excluded_rows = []
