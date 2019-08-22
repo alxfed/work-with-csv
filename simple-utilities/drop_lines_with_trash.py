52fbf8dbd6027272a37db37f210a22b29728fdc5
alxfed <alxfed@gmail.com>
2019-08-22 07:40:32 -0500

    finding values for additional column in another file

--- simple-utilities/drop_lines_with_trash.py
+++ simple-utilities/drop_lines_with_trash.py
@@ -9,28 +9,25 @@ import csv
 read_path = '/media/alxfed/toca/aa-crm/enrich/interior_unprocessed.csv'
 write_path = '/media/alxfed/toca/aa-crm/enrich/interior.csv'
 
-list_of_trash = ['houzz', 'yelp']
+trash = ['houzz', 'yelp']
 
 fieldnames = []
 rows = []
 seen = set()
 duplicates = []
 
-def NotTrash(text, trash_list):
-    for trash in trash_list:
-        if trash in text:
-            return None
-        else:
-            return text
+def NotTrash(text, trash):
+    if trash in text:
+        return None
+    else:
+        return text
 
 with open(read_path) as f:
     f_csv = csv.DictReader(f)
     for row in f_csv:
-        if row['Name'] in seen:
-            duplicates.append(row['Name'])
-        else:
-            seen.add(row['Name'])
+        if NotTrash(row['Website'], trash):
             rows.append(row)
+
     fieldnames = f_csv._fieldnames
 
 print('Duplicates: ', len(duplicates))
