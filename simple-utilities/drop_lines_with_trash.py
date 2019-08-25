97d8e5e92461f3eaf217152d464eb8601d47ba51
alxfed <alxfed@gmail.com>
2019-08-25 12:35:53 -0500

    trash update

--- simple-utilities/drop_lines_with_trash.py
+++ simple-utilities/drop_lines_with_trash.py
@@ -13,7 +13,7 @@ write_path = '/media/alxfed/toca/aa-crm/kb-remodelers/kitchen_and_bath_final.csv
 leftowers_path = '/media/alxfed/toca/aa-crm/kb-remodelers/kitchen_and_bath_leftowers.csv'
 
 
-trash_list = ['houzz', 'yelp', 'yahoo.com', 'lowes.com', 'homedepot', 'facebook',
+trash_list = ['houzz', 'yelp', 'yahoo.com', 'wordpress.com', 'lowes.com', 'homedepot', 'facebook',
               'blogspot', 'wix', 'smithe', 'sbcglobal', 'ymail',
               'mac', 'twitter', 'aol', 'linkedin', 'google', 'gmail',
               'artvan', 'nu-tub', 'https', 'icloud', 'me.com', 'fb.me',
