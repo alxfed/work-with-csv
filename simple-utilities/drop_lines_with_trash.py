77353dacff619b61e4a1f8a97e9c9db55444a06a
alxfed <alxfed@gmail.com>
2019-08-25 11:32:01 -0500

    trash update

--- simple-utilities/drop_lines_with_trash.py
+++ simple-utilities/drop_lines_with_trash.py
@@ -1,22 +1,25 @@
 """
 given: a csv file with a list of objects that may have a value of the
-    property meaning that it is trash
+    property meaning that it is trash. Some lines can have it missing
+    altogether, they should be dumped too.
 needed: a csv file with these lines dropped
 """
 import csv
 
 
-read_path = '/media/alxfed/toca/aa-crm/enrich/kitchen_and_bath_unprocessed.csv'
-write_path = '/media/alxfed/toca/aa-crm/enrich/kitchen_and_bath.csv'
+read_path = '/media/alxfed/toca/aa-crm/kb-remodelers/kitchen_and_bath_unprocessed.csv'
+write_path = '/media/alxfed/toca/aa-crm/kb-remodelers/kitchen_and_bath_processed.csv'
+leftowers_path = '/media/alxfed/toca/aa-crm/kb-remodelers/kitchen_and_bath_leftowers.csv'
 
 
 trash_list = ['houzz', 'yelp', 'lowes', 'homedepot', 'facebook',
               'blogspot', 'wix', 'smithe', 'sbcglobal', 'ymail',
               'mac', 'twitter', 'aol', 'linkedin', 'google', 'gmail',
-              'artvan', 'nu-tub']
+              'artvan', 'nu-tub', 'https.', 'icloud', 'me.com']
 
 fieldnames = []
 rows = []
+lrows = []
 seen = set()
 duplicates = []
 
@@ -29,16 +32,32 @@ def NotTrash(text, trash_list):
 with open(read_path) as f:
     f_csv = csv.DictReader(f)
     for row in f_csv:
-        if NotTrash(row['Website'], trash_list):
-            rows.append(row)
+        if row['Name'] in seen:
+            pass
+        else:
+            if row['Website']:
+                if NotTrash(row['Website'], trash_list):
+                    rows.append(row)
+                    seen.add(row['Name'])
+                else:
+                    lrows.append(row)
+            else:
+                lrows.append(row)
     fieldnames = f_csv._fieldnames
 
 
 write_headers = fieldnames
 write_rows = rows
+leftower_rows = lrows
 
 
 with open(write_path,'w') as f:
     f_csv = csv.DictWriter(f, write_headers)
     f_csv.writeheader()
     f_csv.writerows(write_rows)
+
+with open(leftowers_path,'w') as f:
+    f_csv = csv.DictWriter(f, write_headers)
+    f_csv.writeheader()
+    f_csv.writerows(leftower_rows)
+
